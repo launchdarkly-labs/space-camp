0f3fc9460530ea0b8635ad011118492a9e0371a6
Glitch (gilded-filament) <none>
2020-01-17 17:30:23 +0000

    ✂️🍍 Checkpoint ./public/client.js:9618/765 ./public/styles.css:9618/224

--- public/client.js
+++ public/client.js
@@ -1,8 +1,7 @@
 $(document).keydown(function(event) {
-  console.log(event);
-   if((event.ctrlKey || event.metaKey) && event.which == 75) {
+  if ((event.ctrlKey || event.metaKey) && event.which == 75) {
     event.preventDefault();
-    const KICKASSVERSION = "2.0";
+    // const KICKASSVERSION = "2.0";
     const s = document.createElement("script");
     s.type = "text/javascript";
     document.body.appendChild(s);
