726e4e06e460ac944a26b1973f68444b288868fb
Glitch (ld-space-camp-0) <none>
2020-01-21 19:58:27 +0000

    🍽🎥 Checkpoint ./views/index.html:9618/80068 ./views/base.html:9618/7001 ./views/pricing.html:9618/25210 ./server.py:9618/12 ./views/pricing-freighter.html:9618/55

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 @app.route('/')
 def pricing():
     """Displays the pricing page."""
-    return render_template('index.html')
+    return render_template('pricing.html')
 
 if __name__ == '__main__':
     app.run()
