af82a5459246ae1dc024b1f661b648bf89a8b3c1
Glitch (ld-space-camp-0) <none>
2020-01-22 00:24:17 +0000

    Lesson 1 checkpoint

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# TODO: Add LaunchDarkly to this so we can flag new features
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='public', template_folder='views')
@@ -5,7 +6,9 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 @app.route('/')
 def pricing():
     """Displays the pricing page."""
+    
+    # TODO: Evaluate a feature flag here...
     return render_template('pricing.html')
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
