86b4a4764ec8d8fce89fabf746fe3e8f4c43c818
Glitch (ld-space-camp-0) <none>
2020-01-22 00:04:37 +0000

    🌯🐀 Checkpoint ./public/client.js:9618/801

--- public/client.js
+++ public/client.js
@@ -2,18 +2,16 @@ function r(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-$(function(){
-  $(".btn").click(function(e) {
-    const doc = $(document);
-    confetti({
-      angle: r(55, 125),
-      spread: r(50, 70),
-      particleCount: r(50, 100),
-      origin: {
-          x: e.pageX / doc.width,
-          y: e.pageYT / doc.height,
-      }
-    });
-    e.preventDefault();
+$(".btn").click(function(e) {
+  const doc = $(document);
+  confetti({
+    angle: r(55, 125),
+    spread: r(50, 70),
+    particleCount: r(50, 100),
+    origin: {
+      x: e.pageX / doc.width(),
+      y: e.pageY / doc.height()
+    }
   });
+  e.preventDefault();
 });
